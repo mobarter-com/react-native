601aca7c7b93ea867f31c573d773434b821dc282
mobarter-com <mobarter1@gmail.com>
2025-10-02 22:17:15 +0100

    Flexible Savings Page

---
+++ features/savings/flexible/index.tsx
@@ -0,0 +1,11 @@
+import { Wrapper } from '@/components'
+import { TText } from '@/components'
+import React from 'react'
+
+export default function FlexibleSavingsPage() {
+  return (
+    <Wrapper>
+      <TText>FlexibleSavingsPage</TText>
+    </Wrapper>
+  )
+}
